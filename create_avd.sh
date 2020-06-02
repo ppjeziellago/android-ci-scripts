9ea40d644f74fb5d47d3c2f8209eb88bceca98de
Jeziel Lago <56451104+ppjeziellago@users.noreply.github.com>
2020-06-02 17:20:44 -0300

    Create create_avd.sh

---
+++ create_avd.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+$ANDROID_HOME/platform-tools/adb start-server
+
+echo yes | $ANDROID_HOME/tools/bin/sdkmanager "tools" "system-images;android-28;default;x86"
+
+echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -f -n emulator -k "system-images;android-28;default;x86" --device "Nexus 5"
+
+$ANDROID_HOME/emulator/emulator -avd emulator -no-audio -no-window &

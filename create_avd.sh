b3df824fd4f7c3f6445bc65f25dc2c02faea6f22
Jeziel Lago <jeziellago@gmail.com>
2020-06-03 17:14:05 -0300

    Update scripts

--- create_avd.sh
+++ create_avd.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-$ANDROID_HOME/platform-tools/adb start-server
+$ANDROID_HOME/platform-tools/adb kill-server;
+$ANDROID_HOME/platform-tools/adb start-server;
 
 echo yes | $ANDROID_HOME/tools/bin/sdkmanager "tools" "system-images;android-28;default;x86"
+ 
+$ANDROID_HOME/tools/bin/avdmanager create avd -f -n emulator --abi 'default/x86' --package "system-images;android-28;default;x86" --device "pixel"
 
-echo no | $ANDROID_HOME/tools/bin/avdmanager create avd -f -n emulator -k "system-images;android-28;default;x86" --device "Nexus 5"
-
-$ANDROID_HOME/emulator/emulator -avd emulator -no-audio -no-window -no-snapshot -wipe-data &
+$ANDROID_HOME/emulator/emulator -avd emulator -no-window -noaudio -no-boot-anim -no-snapshot -camera-back none -camera-front none -netfast -accel auto &
--- wait_emulator.sh
+++ wait_emulator.sh
@@ -6,10 +6,9 @@ bootanim=""
 failcounter=0
 timeout_in_sec=600
 
-until [[ "$bootanim" =~ "stopped" ]]; do
-  bootanim=`$ANDROID_HOME/platform-tools/adb -e shell getprop init.svc.bootanim 2>&1 &`
-  if [[ "$bootanim" =~ "device not found" || "$bootanim" =~ "device offline"
-    || "$bootanim" =~ "running" ]]; then
+until [[ "$bootanim" = "1" ]]; do
+  bootanim=`$ANDROID_HOME/platform-tools/adb shell getprop sys.boot_completed 2>&1 &`
+  if [[ "$bootanim" != "1" ]]; then
     let "failcounter += 1"
     echo "Waiting for emulator to start"
     if [[ $failcounter -gt timeout_in_sec ]]; then
@@ -19,5 +18,7 @@ until [[ "$bootanim" =~ "stopped" ]]; do
   fi
   sleep 1
 done
+
+$ANDROID_HOME/platform-tools/adb shell input keyevent 82
 
 echo "Emulator is ready"
